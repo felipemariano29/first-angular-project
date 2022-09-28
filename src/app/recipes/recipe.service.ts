9379959160543782b069f28b49e3f6a5381d31af
Felipe Mariano <felipemariano99@gmail.com>
2022-09-28 14:50:17 -0300

    connected to firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,34 +9,41 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Lasanha',
-      'Fitas colocadas em camadas, e entremeadas com recheio.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1024px-Lasagne_-_stonesoup.jpg',
-      [
-        new Ingredient('Pasta', 1),
-        new Ingredient('Ham', 10),
-        new Ingredient('Cheese', 10),
-        new Ingredient('Meat', 1),
-        new Ingredient('Tomato', 3),
-      ]
-    ),
-    new Recipe(
-      'Estrogonofe',
-      'Cubos de carne servidos num molho de creme de leite.',
-      'https://simplelivingrecipes.com/wp-content/uploads/2020/02/Brazilian-Stroganoff--500x500.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Onion', 3),
-        new Ingredient('Champignon', 10),
-        new Ingredient('Salt', 1),
-        new Ingredient('Tomato', 3)
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Lasanha',
+  //     'Fitas colocadas em camadas, e entremeadas com recheio.',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1024px-Lasagne_-_stonesoup.jpg',
+  //     [
+  //       new Ingredient('Pasta', 1),
+  //       new Ingredient('Ham', 10),
+  //       new Ingredient('Cheese', 10),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Tomato', 3),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Estrogonofe',
+  //     'Cubos de carne servidos num molho de creme de leite.',
+  //     'https://simplelivingrecipes.com/wp-content/uploads/2020/02/Brazilian-Stroganoff--500x500.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Onion', 3),
+  //       new Ingredient('Champignon', 10),
+  //       new Ingredient('Salt', 1),
+  //       new Ingredient('Tomato', 3)
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
